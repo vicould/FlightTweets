faedd8b80733cc8670e2f459f2b4ed2109878f48
Ludovic Delaveau <ldelaveau@gmail.com>
2012-02-11 22:51:09 -0500

    Added some docstrings.

--- src/net/vicould/tweets/TweetFetcher.java
+++ src/net/vicould/tweets/TweetFetcher.java
@@ -11,17 +11,30 @@ import twitter4j.TwitterFactory;
 
 public class TweetFetcher {
 	private TweetSaver dbSaver;
+
+	private TweetSaver getDbSaver() {
+		return dbSaver;
+	}
+	private void setDbSaver(TweetSaver dbSaver) {
+		this.dbSaver = dbSaver;
+	}
 	
 	public TweetFetcher() {
 		setDbSaver(new TweetSaver());
 	}
 	
-	public List<Status> fetchSome(String user, long previousTweetId) {
+	/**
+	 * Fetch some (20) statuses that are older than the id specified as a parameter.
+	 * @param user The user's name from whom to retrieve the tweets.
+	 * @param maxTweetId The upper bound for the tweets (i.e. the method will retrieve tweets with an id lesser than this one).
+	 * @return A list of statuses corresponding to the constraints. Might be empty.
+	 */
+	public List<Status> fetchSome(String user, long maxTweetId) {
 		Twitter twitter = new TwitterFactory().getInstance();
 		
 		try {
 			Paging paging = new Paging();
-			paging.maxId(previousTweetId - 1L);
+			paging.maxId(maxTweetId - 1L);
 			
 			List<Status> statuses = twitter.getUserTimeline(user, paging);
 			for (Status status : statuses) {
@@ -36,6 +49,12 @@ public class TweetFetcher {
 		return new ArrayList<Status>();
 	}
 	
+	/**
+	 * Fetch a set of tweets for the specified user, inside the specified ids.
+	 * @param user The name of the user.
+	 * @param newestTweetId The upper bound for the interval of tweets.
+	 * @param oldestTweetId The lower bound.
+	 */
 	public void fetch(String user, long newestTweetId, long oldestTweetId) {
 		long currentId = newestTweetId;
 		List<Status> results;
@@ -47,14 +66,6 @@ public class TweetFetcher {
 				getDbSaver().saveTweets(results);
 			}
 		} while (currentId > oldestTweetId);
-	}
-
-	private TweetSaver getDbSaver() {
-		return dbSaver;
-	}
-
-	private void setDbSaver(TweetSaver dbSaver) {
-		this.dbSaver = dbSaver;
 	}
 
 }
