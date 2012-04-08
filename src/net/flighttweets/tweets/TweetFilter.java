c2f5d711368a38b07e48f546e416fd960cee41c0
Ian Mathew <imathew43@gmail.com>
2012-04-08 17:42:05 -0400

    Committed latest changes.

--- src/net/flighttweets/tweets/TweetFilter.java
+++ src/net/flighttweets/tweets/TweetFilter.java
@@ -79,54 +79,5 @@ public class TweetFilter {
                     e.printStackTrace();
                 }
             }
-
-	public void saveTweets(List<Status> statusesToSave) {
-		try {
-			Connection connection = StorageManager.getInstance().getConnection();
-			PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO TWEETS VALUES (?, ?, ?, ?, ?)");
-			
-			for (Status status : statusesToSave) {
-				insertStatement.setLong(1, status.getId());
-				insertStatement.setString(2, status.getUser().getScreenName());
-				insertStatement.setLong(3, status.getUser().getId());
-				insertStatement.setString(4, status.getText());
-				// conversion between a java.util.Date and a java.sql.Date
-				insertStatement.setDate(5, new java.sql.Date(status.getCreatedAt().getTime()));
-				insertStatement.addBatch();
-			}
-			insertStatement.executeBatch();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Saves all the tweets to a CSV file.
-	 * @param filename The filename to save to, which will be placed in the results directory.
-	 */
-	public void saveTweetsToFile(String filename) {
-		File file = new File("results");
-		if (!file.exists()) {
-			file.mkdir();
-		}
-		
-		try {
-			Writer output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("results/" + filename), Charset.forName("UTF-8")));
-			
-			Connection connection = StorageManager.getInstance().getConnection();
-			PreparedStatement retrievalStatement = connection.prepareStatement("SELECT * FROM TWEETS ORDER BY TWEET_ID");
-			ResultSet results = retrievalStatement.executeQuery();
-
-			output.write("Tweet id, User id, Tweet, Created at\n");
-			while (results.next()) {
-				output.write("\"" + results.getString(1) + "\",\"" + results.getString(2) + "\",\"" + results.getString(3)  +"\",\"" + results.getString(4) + "\"\n");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 }
+
